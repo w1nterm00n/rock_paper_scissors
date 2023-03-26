5d348166ee07712545514d117f1c66333545ac7c
w1nterm00n <kordesovad@gmail.com>
2023-03-26 20:47:26 +0300

    changed all of console.logs into DOM methods and added styles a bit^^

--- script.js
+++ script.js
@@ -6,48 +6,61 @@ function getComputerChoice () {
     return (arrayOfOptions[randomNumber]);
 }
 
-//запрашивает выбор пользователя
-function getUserChoice () {
-    let userChoice = prompt ('rock, paper or scissors?');
-    userChoice = userChoice.toLowerCase();
-    return userChoice;
-}
+//привязываю кнопки
+    const rockButton = document.querySelector('.rock');
+    const paperButton = document.querySelector('.paper');
+    const scissorsButton = document.querySelector('.scissors');
+const resultTable = document.querySelector('.resultTable');
+const counterTable = document.querySelector('.counterTable');
 
-var userPoints = 0;
-var computerPoints = 0;
+//вешаю обработчики событий на кнопки
+    rockButton.addEventListener('click', () => {        
+        playRound (getComputerChoice(), "rock"); 
+    });     
+    paperButton.addEventListener('click', () => {        
+        playRound (getComputerChoice(), "paper"); 
+    }); 
+    scissorsButton.addEventListener('click', () => {
+        playRound (getComputerChoice(), "scissors"); 
+    });  
+
+
+let userPoints = 0;
+let computerPoints = 0;
+let playRoundCounter = 0;
 //проверяет и выводит кто выиграл/проиграл
 function playRound (computerSelection, playerSelection) {
+    playRoundCounter += 1;
     if (computerSelection == playerSelection) {
-        console.log("dead heat!");
-        return 0;
+        counterTable.textContent = "dead heat!";
     };
     if ( (computerSelection == 'rock' && playerSelection == 'scissors') ||
         (computerSelection == 'scissors' && playerSelection == 'paper') ||
         (computerSelection == 'paper' && playerSelection == 'rock') ) {
-            console.log( `You Lose! ${computerSelection} beats ${playerSelection}` );
+            counterTable.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
             computerPoints += 1;
         } else {
-            console.log( `You Win! ${playerSelection} beats ${computerSelection}` );
+            counterTable.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
             userPoints += 1;
+        }
+        if (playRoundCounter > 4) {
+            isThisTheEnd ();
         }
 }
 
-//запускает пять ходов и выводит результат
-function game () {
-    for (let i=0; i < 5; i++) {
-        playRound (getComputerChoice(), getUserChoice());
-    }
-    console.log( `Your points: ${userPoints}, computer points: ${computerPoints}`);
+
+function isThisTheEnd () {
+    counterTable.textContent = `Your points: ${userPoints}, computer points: ${computerPoints}`;
+
     if (userPoints > computerPoints) {
-        console.log("You win!");
+        resultTable.textContent = "Finally you win!";
     } else if (computerPoints > userPoints) {
-        console.log("You lose!");
+        resultTable.textContent = "Finally you lose!";
     } else {
-        console.log("Dead heat!")
+        resultTable.textContent = "Finally dead heat!";
     };
     //обнуляю счётчики в конце
     userPoints = 0;
     computerPoints = 0;
+    playRoundCounter = 0;
 }
-
-game ();
